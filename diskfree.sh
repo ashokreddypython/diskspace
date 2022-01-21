99d9b9a7731193102226a3115ca9665d105166b8
ashokreddypython <70519289+ashokreddypython@users.noreply.github.com>
2022-01-21 11:04:57 +0530

    Update diskfree.sh

--- diskfree.sh
+++ diskfree.sh
@@ -4,7 +4,7 @@ do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
-  if [ $usep -ge 60 ]; then
+  if [ $usep -ge 70 ]; then
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" |
      mail -s "Alert: Almost out of disk space $usep%" you@somewhere.com
   fi
